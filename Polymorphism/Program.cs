86b66833855c9cecdb620b1e8831d07e78b1543f
Gökberk Hayali <gokberkhayali@gmail.com>
2022-01-24 22:42:30 +0100

    polymorphism created

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Polymorphism;
+
+Console.WriteLine("Hello, World!");
+
+
+BaseClass bc = new BaseClass();
+DerivedClass dc = new DerivedClass();
+BaseClass bcdc = new DerivedClass();
+
+class BaseClass
+{
+    public void TakeFunction (Func<string,int> test)
+    {
+        var aaa = test;
+        
+    }
+    public void Method1()
+    {
+        Console.WriteLine("Base - Method1");
+    }
+
+}
+
+class DerivedClass : BaseClass
+{
+    public void Method2()
+    {
+        Console.WriteLine("Derived - Method2");
+    }
+}
